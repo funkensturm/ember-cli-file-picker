dfdebda1418a2e8f72827feacbc13af0743c1f30
Manuel Wiedenmann <manuel@funkensturm.de>
2015-04-22 00:08:49 +0200

    add test

--- tests/unit/components/file-picker-test.js
+++ tests/unit/components/file-picker-test.js
@@ -4,6 +4,10 @@ import {
   test
 } from 'ember-qunit';
 
+const {
+  get: get
+} = Ember;
+
 moduleForComponent('file-picker', 'FilePickerComponent');
 
 test('it renders', function(assert) {
@@ -16,4 +20,17 @@ test('it renders', function(assert) {
   // Renders the component to the page
   this.render();
   assert.equal(component._state, 'inDOM');
+});
+
+test('it has correct defaults', function(assert) {
+  assert.expect(6);
+
+  var component = this.subject();
+
+  assert.equal(get(component, 'accept'), '*');
+  assert.equal(get(component, 'multiple'), false);
+  assert.equal(get(component, 'preview'), true);
+  assert.equal(get(component, 'dropzone'), true);
+  assert.equal(get(component, 'progress'), true);
+  assert.equal(get(component, 'readAs'), 'readAsFile');
 });
