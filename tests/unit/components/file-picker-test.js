8d19ca628902300ea758c97173e4e011f88f1c91
Manuel Wiedenmann <manuel@funkensturm.de>
2015-04-22 00:02:18 +0200

    add basic test

---
+++ tests/unit/components/file-picker-test.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+import {
+  moduleForComponent,
+  test
+} from 'ember-qunit';
+
+moduleForComponent('file-picker', 'FilePickerComponent');
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Creates the component instance
+  var component = this.subject();
+  assert.equal(component._state, 'preRender');
+
+  // Renders the component to the page
+  this.render();
+  assert.equal(component._state, 'inDOM');
+});
