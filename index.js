8240324ad339dcc499f2843bcd63083d2d3a46e7
Craig Sketchley <craigsketchley@gmail.com>
2017-05-30 22:25:14 +1000

    Updated ember.

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-/* jshint node: true */
+/* eslint-env node */
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-file-picker',
-
-  included: function included(app) {
-    this._super.included(app);
-
-    var isProduction = app.env === 'production';
-    if (!isProduction) {
-      app.import(app.bowerDirectory + '/blob/Blob.js', { type: 'test' });
-    }
-  }
+  name: 'ember-cli-file-picker'
 };
